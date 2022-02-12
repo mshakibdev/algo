197e82d19cd551cf26e1d4b9889e0ff13827201a
Md. Shakib Hassan <mshassan.dhk313@gmail.com>
2022-02-12 12:16:46 +0600

    comments added

--- selectionSort.js
+++ selectionSort.js
@@ -1,12 +1,13 @@
 function selectionSort(array) {
     for (let index = 0; index < array.length - 1; index++) {
-
+        //assume
         let smallestIndex = index;
         for (let nextIndex = index + 1; nextIndex < array.length; nextIndex++) {
             if (array[nextIndex] < array[smallestIndex]) {
                 smallestIndex = nextIndex;
             }
         }
+        // swapping
         [array[index], array[smallestIndex]] = [array[smallestIndex], array[index]];
     }
     return array;
